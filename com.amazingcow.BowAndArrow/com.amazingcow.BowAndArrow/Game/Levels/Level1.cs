e99c076ae9f1565b8944bfe06404ad47fcd87ea1
n2omatt <n2o.matt@gmail.com>
2016-04-01 03:21:14 -0300

    [WIP] Continue the implementation. Add region surrounding the usings.

--- com.amazingcow.BowAndArrow/com.amazingcow.BowAndArrow/Game/Levels/Level1.cs
+++ com.amazingcow.BowAndArrow/com.amazingcow.BowAndArrow/Game/Levels/Level1.cs
@@ -1,57 +1,109 @@
-﻿using System;
+﻿#region Usings
+//System
+using System;
+using System.Diagnostics;
+//Xma
 using Microsoft.Xna.Framework;
-using System.Runtime.InteropServices;
 using Microsoft.Xna.Framework.Input;
+#endregion //Usings
+
 
 namespace com.amazingcow.BowAndArrow
 {
     public class Level1 : Level
     {
-        public Level1() : 
+        public Level1() :
             base()
         {
-            
             for(int i = 10; i < 20; ++i)
             {
-                Enemies.Add(new RedBalloon() {
-                        Position = new Vector2(i * 30, 100)
-                });
+                var balloon = new RedBalloon(new Vector2(i * 30, 100));
+                balloon.OnStateChangeDead  += OnEnemyStateChangeDead;
+                balloon.OnStateChangeDying += OnEnemyStateChangeDying;
+
+                Enemies.Add(balloon);
             }
 
-            for(int i = 20; i < 30; ++i)
-            {
-                Enemies.Add(new YellowBalloon() {
-                        Position = new Vector2(i * 30, 100)
-                });
-            }
+            Player = new Archer(new Vector2(100, 100));
         }
 
-        #region Update / Draw 
+        void OnEnemyStateChangeDying(object sender, EventArgs e)
+        {
+            var gameObj = sender as Enemy;
+            gameObj.OnStateChangeDying -= OnEnemyStateChangeDying;
+
+            Debug.WriteLine("Enemy ChangeState - Dying");
+        }
+
+        void OnEnemyStateChangeDead(object sender, EventArgs e)
+        {
+            var gameObj = sender as Enemy;
+            gameObj.OnStateChangeDead -= OnEnemyStateChangeDead;
+
+            Debug.WriteLine("Enemy ChangeState - Dead");
+        }
+
+        void OnArrowStateChangedDead(object sender, EventArgs e)
+        {
+            var arrow = sender as Arrow;
+            arrow.OnStateChangeDead -= OnArrowStateChangedDead;
+
+            Debug.WriteLine("Arrow ChangeState - Dead - {0}", PlayerArrows.Count);
+        }
+
+
+        #region Update / Draw
         public override void Update(GameTime gt)
         {
             var state = Mouse.GetState();
             var pressed = state.LeftButton == ButtonState.Pressed;
-            var pos     = new Vector2(state.X, state.Y);
+            var pos = new Vector2(state.X, state.Y);
 
-            if(pressed)
-                PlayerArrows.Add(new Arrow() 
-                {
-                    Position = pos
-                });
+//            if(pressed)
+//            {
+//                var arrow = new Arrow(pos);
+//                arrow.OnStateChangeDead += OnArrowStateChangedDead;
+//                PlayerArrows.Add(arrow);
+//            }
 
             //Enemies
-            foreach(var enemy in Enemies) 
+            for(int i = Enemies.Count - 1; i >= 0; --i)
             {
-                if(enemy.CurrentSprite.BoundingBox.Contains(pos))
-                    enemy.Kill();
+                var enemy = Enemies[i];
+                if(enemy.CurrentState == GameObject.State.Dead)
+                {
+                    Enemies.RemoveAt(i);
+                    break;
+                }
 
                 enemy.Update(gt);
             }
 
             //Arrows
-            foreach(var arrow in PlayerArrows)
-                arrow.Update(gt);
+            for(int i = PlayerArrows.Count - 1; i >= 0; --i)
+            {
+                var arrow = PlayerArrows[i];
+                if(arrow.CurrentState == GameObject.State.Dead)
+                {
+                    PlayerArrows.RemoveAt(i);
+                    break;
+                }
 
+                arrow.Update(gt);
+            }
+
+            foreach(var enemy in Enemies)
+            {
+                foreach(var arrow in PlayerArrows)
+                {
+                    if(enemy.CheckCollisionArrow(arrow))
+                        enemy.Kill();
+                }
+
+                //COWTODO: Player hit.
+            }
+
+            Player.Update(gt);
         }
         public override void Draw(GameTime gt)
         {
@@ -63,8 +115,10 @@ namespace com.amazingcow.BowAndArrow
             foreach(var arrow in PlayerArrows)
                 arrow.Draw(gt);
 
+            //Player.
+            Player.Draw(gt);
         }
-        #endregion //Update / Draw 
+        #endregion //Update / Draw
 
 
         #region Load / Unload
