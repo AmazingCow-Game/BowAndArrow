3d07c64533968f7d5e0806004dce30777934187a
n2omatt <n2o.matt@gmail.com>
2016-04-01 03:19:55 -0300

    Change the newline to unix. Change the whole object model.

--- com.amazingcow.BowAndArrow/com.amazingcow.BowAndArrow/GameFramework/GameObject.cs
+++ com.amazingcow.BowAndArrow/com.amazingcow.BowAndArrow/GameFramework/GameObject.cs
@@ -1,1 +1,163 @@
-﻿#region Usings//System using System;//XNAusing Microsoft.Xna.Framework;using Microsoft.Xna.Framework.Audio;#endregion //Usingsnamespace com.amazingcow.BowAndArrow{    public abstract class GameObject    {        #region Enums         public enum State         {            Alive,            Dying,            Dead         }        #endregion //Enums        #region Public Properties        public virtual Vector2 Position      { get; set; }         public virtual Vector2 Speed         { get; set; }        public virtual Sprite  CurrentSprite { get; protected set; }        public virtual State   CurrentState  { get; protected set; }        #endregion //Public Properties        #region CTOR         public GameObject()        {            CurrentSprite = null;            CurrentState  = State.Alive;        }        #endregion //CTOR         #region Update / Draw         public abstract void Update(GameTime gt);        public abstract void Draw(GameTime gt);        #endregion //Update / Draw         #region Public Methods         public abstract void Kill();        #endregion //Public Methods     }}
+﻿#region Usings
+//System
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+//XNA
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+#endregion //Usings
+
+
+namespace com.amazingcow.BowAndArrow
+{
+    public abstract class GameObject
+    {
+        #region Constants
+        public const int kAliveTexturesListHintSize = 2;
+        public const int kDyingTexturesListHintSize = 1;
+        #endregion //Constants
+
+
+        #region Events
+        public event EventHandler<EventArgs> OnStateChangeDying;
+        public event EventHandler<EventArgs> OnStateChangeDead;
+        #endregion //Events
+
+
+        #region Enums
+        public enum State
+        {
+            Alive,
+            Dying,
+            Dead
+        }
+        #endregion //Enums
+
+        #region iVars
+        State _currentState;
+        #endregion iVars
+
+
+        #region Public Properties
+        public Vector2 Position
+        { get; protected set; }
+
+        public Vector2 Speed
+        { get; protected set; }
+
+        public Rectangle BoundingBox
+        {
+            get {
+                return new Rectangle((int)Position.X,
+                                     (int)Position.Y,
+                                     CurrentTexture.Width,
+                                     CurrentTexture.Height);
+            }
+        }
+
+        public virtual Rectangle HitBox
+        {
+            get {
+                return BoundingBox;
+            }
+        }
+
+        public State CurrentState
+        {
+            get {
+                return _currentState;
+            }
+            protected set {
+                ChangeState(value);
+            }
+        }
+
+        public List<Texture2D> AliveTexturesList
+        { get; protected set; }
+
+        public List<Texture2D> DyingTexturesList
+        { get; protected set; }
+
+        public Texture2D CurrentTexture
+        {
+            get {
+                return CurrentTexturesList[CurrentTextureIndex];
+            }
+        }
+
+        public List<Texture2D> CurrentTexturesList
+        { get; protected set; }
+
+        public int CurrentTextureIndex
+        { get; protected set; }
+
+        public Clock TextureAnimationTimer
+        { get; protected set; }
+
+        #endregion //Public Properties
+
+
+        #region CTOR
+        public GameObject(Vector2 position, Vector2 speed,
+                          float msToChangeTexture)
+        {
+            Position     = position;
+            Speed        = speed;
+            CurrentState = State.Alive;
+
+            AliveTexturesList     = new List<Texture2D>(kAliveTexturesListHintSize);
+            DyingTexturesList     = new List<Texture2D>(kDyingTexturesListHintSize);
+            CurrentTexturesList   = AliveTexturesList;
+            CurrentTextureIndex   = 0;
+            TextureAnimationTimer = new Clock(msToChangeTexture,
+                                              Clock.kRepeatForever);
+        }
+        #endregion //CTOR
+
+
+        #region Update / Draw
+        public virtual void Update(GameTime gt)
+        {
+
+        }
+        public virtual void Draw(GameTime gt)
+        {
+            if(CurrentState == State.Dead)
+                return;
+
+            GameManager.Instance.CurrentSpriteBatch.Draw(
+                CurrentTexture,
+                Position
+            );
+        }
+        #endregion //Update / Draw
+
+
+        #region Public Methods
+        public abstract void Kill();
+        #endregion //Public Methods
+
+
+        #region Private Methods
+        private void ChangeState(State state)
+        {
+            _currentState = state;
+
+            if(_currentState == State.Dead)
+            {
+                OnStateChangeDead(this, EventArgs.Empty);
+            }
+            else if(_currentState == State.Dying)
+            {
+                OnStateChangeDying(this, EventArgs.Empty);
+                CurrentTexturesList = DyingTexturesList;
+                CurrentTextureIndex = 0;
+            }
+        }
+        #endregion //Private Methods
+
+    }//class GameObject
+}//namespace com.amazingcow.BowAndArrow
+
