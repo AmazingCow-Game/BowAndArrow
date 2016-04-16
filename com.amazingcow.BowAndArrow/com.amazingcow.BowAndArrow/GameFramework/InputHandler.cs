f5edcc13330950e7b135681dc8d802f86b093f7c
n2omatt <n2o.matt@gmail.com>
2016-04-16 08:56:24 -0300

    - Start implementation.

---
+++ com.amazingcow.BowAndArrow/com.amazingcow.BowAndArrow/GameFramework/InputHandler.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+
+
+namespace com.amazingcow.BowAndArrow
+{
+	public class ActionStep
+	{
+		public int Button;
+		public ButtonState State;
+
+		public bool Check(MouseState state)
+		{
+			return state.LeftButton == State;
+		}
+
+		public override string ToString ()
+		{
+			return string.Format("[ActionStep] Button:{0} - State:{1}", 
+								 Button, State);
+		}
+	}
+
+	public class Action
+	{
+		public List<ActionStep> Steps = new List<ActionStep>();
+		public int StepIndex = 0;
+
+		public event EventHandler<EventArgs> OnTrigger;
+
+		public void Check(MouseState state)
+		{					
+		}
+	}
+
+	public class InputHandler
+	{
+		#region Singleton 
+		private static InputHandler s_instance = null;
+		public static InputHandler Instance
+		{
+			get { 
+				if(s_instance == null)
+					s_instance = new InputHandler();
+				return s_instance;
+			}
+		}
+		#endregion
+
+		public List<Action> Actions = new List<Action>();
+
+		#region CTOR 
+		private InputHandler()
+		{
+		}
+		#endregion
+
+
+		#region Update 
+		public void Update()
+		{				
+			var state = Mouse.GetState();
+
+			foreach(var action in Actions)
+				action.Check(state);
+		}
+		#endregion
+
+	}
+}
+
