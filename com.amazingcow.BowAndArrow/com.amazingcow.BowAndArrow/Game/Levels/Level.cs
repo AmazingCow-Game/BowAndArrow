6ed8d386698f4f951ef5da4cf21f6454d764f007
n2omatt <n2o.matt@gmail.com>
2016-04-16 08:52:20 -0300

    - Remove the unneeded usings. - Sort and organize the usings. - Cosmetic changes. - Add some constants. - Add some properties. - Update the CTOR. - Add comments.

--- com.amazingcow.BowAndArrow/com.amazingcow.BowAndArrow/Game/Levels/Level.cs
+++ com.amazingcow.BowAndArrow/com.amazingcow.BowAndArrow/Game/Levels/Level.cs
@@ -1,47 +1,74 @@
-﻿using System;
-using Microsoft.Xna.Framework;
+﻿#region Usings
+//System
+using System;
 using System.Collections.Generic;
-using OpenTK.Platform.MacOS;
+//XNA
+using Microsoft.Xna.Framework;
+#endregion //Usings
+
 
 namespace com.amazingcow.BowAndArrow
 {
     public abstract class Level
     {
-        public enum State 
+        #region Enums / Constants
+        public enum State
         {
-            Intro,
-            Playing,
-            Paused,
-            GameOver
+            Intro,   //
+            Playing, //
+            Paused,  //
+            GameOver //
         }
 
-
-        #region Public Properties 
-        public State        CurrentState { get; protected set; }
-        public List<Enemy>  Enemies      { get; protected set; }
-        public Archer       Player       { get; protected set; }
-        public List<Arrow>  PlayerArrows { get; protected set; }
-        #endregion //Public Properties 
+        private const int kEnemiesHintCount = 20;
+        private const int kPapersHintCount  =  3;
+        #endregion //Enums / Constants
 
 
+        #region Public Properties
+        public State CurrentState
+        { get; protected set; }
+
+        public List<Enemy> Enemies
+        { get; protected set; }
+
+        public Archer Player
+        { get; protected set; }
+
+        public List<Arrow> PlayerArrows
+        { get; protected set; }
+
+        public List<Paper> Papers
+        { get; protected set; }
+        #endregion //Public Properties
+
+
+        #region CTOR
         public Level()
         {
             CurrentState = State.Intro;
-            Enemies      = new List<Enemy>();
-            PlayerArrows = new List<Arrow>();
+
+            //Do the basic initialization.
+            //So derived classes don't need to
+            //init this basic stuff...
+            Enemies      = new List<Enemy>(kEnemiesHintCount);
+            PlayerArrows = new List<Arrow>(Archer.kMaxArrowsCount);
+            Papers       = new List<Paper>(kPapersHintCount);
         }
+        #endregion //CTOR
 
 
-        #region Update / Draw 
+        #region Update / Draw
         public abstract void Update(GameTime gt);
         public abstract void Draw(GameTime gt);
-        #endregion //Update / Draw 
+        #endregion //Update / Draw
 
 
         #region Load / Unload
         public abstract void Load();
         public abstract void Unload();
         #endregion //Load / Unload
-    }
-}
+
+    }//class Level
+}//namespace com.amazingcow.BowAndArrow
 
