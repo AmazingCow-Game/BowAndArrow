0283cdfdeaf1d7b8bca62c07e2d9502015c0dde1
n2omatt <n2o.matt@gmail.com>
2016-05-05 21:59:19 -0300

    - Start the text rendering code.

--- com.amazingcow.BowAndArrow/com.amazingcow.BowAndArrow/Game/IntroPaper.cs
+++ com.amazingcow.BowAndArrow/com.amazingcow.BowAndArrow/Game/IntroPaper.cs
@@ -12,21 +12,26 @@ namespace com.amazingcow.BowAndArrow
     public class Paper : GameObject
     {
         #region iVars
-        private string _title;
-        private string _contents;
+        string _title;
+        string _contents;
+
+        SpriteFont _spriteFont;
         #endregion //iVars
 
         #region CTOR
         public Paper(string title, string contents) :
             base(Vector2.Zero, Vector2.Zero, 0)
         {
-            //Init the iVars...
+            //Init the iVars.
             _title    = title;
             _contents = contents;
 
-            //Init the Textures...
+            //Init the Textures.
             var resMgr = ResourcesManager.Instance;
             AliveTexturesList.Add(resMgr.GetTexture("paper"));
+                      
+            //Init the Sprite Fonts.
+            _spriteFont = resMgr.GetFont("coolvetica");
 
             //Set the position to center of screen.
             var viewport = GameManager.Instance.GraphicsDevice.Viewport;
@@ -35,6 +40,8 @@ namespace com.amazingcow.BowAndArrow
             var y = (viewport.Height / 2) - (BoundingBox.Height / 2);
 
             Position = new Vector2(x, y);
+
+
         }
         #endregion
 
@@ -44,6 +51,30 @@ namespace com.amazingcow.BowAndArrow
             //Do nothing...
         }
 
+
+        public override void Draw (GameTime gt)
+        {
+            base.Draw(gt);
+        
+            var sbatch       = GameManager.Instance.CurrentSpriteBatch;
+            var paperCenterX = BoundingBox.Center.X;
+            var paperTopY    = BoundingBox.Top;
+
+            var splitedString = _contents.Split('\n');
+            for(int i = 1; i < splitedString.Length; i++)
+            {
+                var currStr = splitedString[i];
+                var strSize = _spriteFont.MeasureString(currStr);
+    
+                var pos = new Vector2(paperCenterX - (strSize.X / 2),
+                                      paperTopY);
+                
+                pos.Y += strSize.Y * i;
+                sbatch.DrawString(_spriteFont, currStr, pos, Color.Black);
+            }
+
+        
+        }
     }//class IntroPaper
 }//namespace com.amazingcow.BowAndArrow
 
