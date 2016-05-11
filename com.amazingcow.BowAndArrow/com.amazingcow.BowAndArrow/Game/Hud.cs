d7b454331a4dbe3658b2bb7bc6c4d065a11fbb0b
n2omatt <n2o.matt@gmail.com>
2016-05-11 02:17:26 -0300

    - Rewrite the hud drawing.   So now window can be of any reasonable size.

--- com.amazingcow.BowAndArrow/com.amazingcow.BowAndArrow/Game/Hud.cs
+++ com.amazingcow.BowAndArrow/com.amazingcow.BowAndArrow/Game/Hud.cs
@@ -15,14 +15,31 @@ namespace com.amazingcow.BowAndArrow
     {
         #region Constants
         const int kPaddingToBackground = 6;
+        const int kBackgroundHeight    = 46;
         #endregion //Constants
+
+
+        #region Properties
+        public override Rectangle BoundingBox
+        {
+            get {
+                return new Rectangle((int)Position.X,
+                                     (int)Position.Y,
+                                     _lvl.WindowRect.Width,
+                                     kBackgroundHeight);
+            }
+        }
+        #endregion //Public Properties
 
 
         #region iVars
         Level      _lvl;
         SpriteFont _spriteFont;
         Texture2D  _littleArrowTexture;
-        Texture2D  _hudBackgroundTexture;
+
+        Texture2D _hudLeft;
+        Texture2D _hudCenter;
+        Texture2D _hudRight;
         #endregion //iVars
 
 
@@ -35,11 +52,12 @@ namespace com.amazingcow.BowAndArrow
             //Init the Textures.
             var resMgr = ResourcesManager.Instance;
 
-            _hudBackgroundTexture = resMgr.GetTexture("hud");
+            _hudLeft   = resMgr.GetTexture("hud_left");
+            _hudCenter = resMgr.GetTexture("hud_center");
+            _hudRight  = resMgr.GetTexture("hud_right");
+
             _littleArrowTexture   = resMgr.GetTexture("little_arrow");
 
-            AliveTexturesList.Add(_hudBackgroundTexture);
-                        
             //Init the Sprite Fonts.
             _spriteFont = resMgr.GetFont("coolvetica");
         }
@@ -56,82 +74,102 @@ namespace com.amazingcow.BowAndArrow
                       
         #region Draw
         public override void Draw(GameTime gt)
-        {
-            base.Draw(gt);        
+        {            
+            var sb = GameManager.Instance.CurrentSpriteBatch;
 
-            var sb     = GameManager.Instance.CurrentSpriteBatch;
-            var bounds = _hudBackgroundTexture.Bounds;
-
-            DrawLevelTitle      (sb, bounds);
-            DrawLevelDescription(sb, bounds);
-            DrawScore           (sb, bounds);
-            DrawHighScore       (sb, bounds);
-            DrawArrowsCount     (sb, bounds);
-            DrawArrowsIcons     (sb, bounds);
+            DrawBackground      (sb);
+            DrawLevelTitle      (sb);
+            DrawLevelDescription(sb);
+            DrawScore           (sb);
+            DrawHighScore       (sb);
+            DrawArrowsCount     (sb);
+            DrawArrowsIcons     (sb);
         }
 
 
-        void DrawLevelTitle(SpriteBatch sb, Rectangle bounds)
+
+        void DrawBackground(SpriteBatch sb)
+        {
+            //Left
+            sb.Draw(_hudLeft, new Vector2(BoundingBox.Left, BoundingBox.Top));
+            //Center
+            sb.Draw(_hudCenter, 
+                    new Vector2(_hudLeft.Width, BoundingBox.Top),
+                    null, 
+                    null,
+                    null, 
+                    0, 
+                    new Vector2(BoundingBox.Width - _hudRight.Width, 1),
+                    null,
+                    SpriteEffects.None,
+                    0);
+            //Right                   
+            sb.Draw(_hudRight, 
+                     new Vector2(BoundingBox.Right - _hudRight.Width, 
+                                 BoundingBox.Top));                           
+        }
+
+        void DrawLevelTitle(SpriteBatch sb)
         {
             var name = _lvl.LevelTitle;
             var size = _spriteFont.MeasureString(name);
-            var pos  =  new Vector2(bounds.Center.X - (size.X / 2),
-                                    bounds.Top + kPaddingToBackground);
+            var pos  =  new Vector2(BoundingBox.Center.X - (size.X / 2),
+                                    BoundingBox.Top + kPaddingToBackground);
 
             sb.DrawString(_spriteFont, name, pos, Color.Black);
         }
 
-        void DrawLevelDescription(SpriteBatch sb, Rectangle bounds)
+        void DrawLevelDescription(SpriteBatch sb)
         {
             var desc = _lvl.LevelDescription;
             var size = _spriteFont.MeasureString(desc);
-            var pos  =  new Vector2(bounds.Center.X - (size.X / 2),
-                                    bounds.Bottom - size.Y - kPaddingToBackground);
+            var pos  =  new Vector2(BoundingBox.Center.X - (size.X / 2),
+                                    BoundingBox.Bottom - size.Y - kPaddingToBackground);
 
             sb.DrawString(_spriteFont, desc, pos, Color.Black);
         }
 
-        void DrawScore(SpriteBatch sb, Rectangle bounds)
+        void DrawScore(SpriteBatch sb)
         {
             var score = String.Format("Score: {0}",
                                       GameManager.Instance.HighScore);
 
-            var pos   = new Vector2(bounds.Left + kPaddingToBackground, 
-                                    bounds.Top  + kPaddingToBackground);
+            var pos   = new Vector2(BoundingBox.Left + kPaddingToBackground, 
+                                    BoundingBox.Top  + kPaddingToBackground);
 
             sb.DrawString(_spriteFont, score, pos, Color.Black);
         }
 
-        void DrawHighScore(SpriteBatch sb, Rectangle bounds)
+        void DrawHighScore(SpriteBatch sb)
         {
             var score = String.Format("High Score: {0}", 
                                       GameManager.Instance.HighScore);
 
             var size  = _spriteFont.MeasureString(score);
-            var pos   = new Vector2(bounds.Left   + kPaddingToBackground, 
-                                    bounds.Bottom - kPaddingToBackground - size.Y);
+            var pos   = new Vector2(BoundingBox.Left   + kPaddingToBackground, 
+                                    BoundingBox.Bottom - kPaddingToBackground - size.Y);
 
             sb.DrawString(_spriteFont, score, pos, Color.Black);
         }
 
-        void DrawArrowsCount(SpriteBatch sb, Rectangle bounds)
+        void DrawArrowsCount(SpriteBatch sb)
         {
             var count = String.Format("Arrows: {0}", _lvl.Player.ArrowsCount);
             var size  = _spriteFont.MeasureString(count);
-            var pos   = new Vector2(bounds.Right - size.X - kPaddingToBackground, 
-                                    bounds.Top   + kPaddingToBackground);
+            var pos   = new Vector2(BoundingBox.Right - size.X - kPaddingToBackground, 
+                                    BoundingBox.Top   + kPaddingToBackground);
 
             sb.DrawString(_spriteFont, count, pos, Color.Black);
         }
 
-        void DrawArrowsIcons(SpriteBatch sb, Rectangle bounds)
+        void DrawArrowsIcons(SpriteBatch sb)
         {
             var size = _littleArrowTexture.Bounds;
 
             for(int i = _lvl.Player.ArrowsCount; i > 0; --i)
             {
-                var x = (bounds.Right - kPaddingToBackground) - (i * size.Width);
-                var y = bounds.Bottom - kPaddingToBackground - size.Height;
+                var x = (BoundingBox.Right - kPaddingToBackground) - (i * size.Width);
+                var y = BoundingBox.Bottom - kPaddingToBackground - size.Height;
 
                 sb.Draw(_littleArrowTexture, new Vector2(x, y), Color.White);
             }
